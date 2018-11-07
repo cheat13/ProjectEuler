661823c980f2ea578b721e39abb83f07263b08c6
Kritsana <tragoolphrom@gmail.com>
2018-11-07 12:32:35 +0700

    refacter problem002

--- ProjectEuler.ConsoleApp/Calculate.cs
+++ ProjectEuler.ConsoleApp/Calculate.cs
@@ -11,25 +11,25 @@ namespace ProjectEuler.ConsoleApp
             var result = sequence.Where(it => it % 2 == 0).Sum();
             return result;
         }
-        
+
         public int[] FindSequence(int number)
         {
             var sequence = new List<int>();
             sequence.Add(1);
             sequence.Add(2);
 
-            for (int i = 2; ; i++)
+            for (int i = 0; ; i++)
             {
-                if (sequence[i - 1] + sequence[i - 2] <= number)
+                if (sequence[i + 1] + sequence[i] <= number)
                 {
-                    sequence.Add(sequence[i - 1] + sequence[i - 2]);
+                    sequence.Add(sequence[i + 1] + sequence[i]);
                 }
                 else
                 {
                     break;
                 }
             }
-            
+
             return sequence.ToArray();
         }
     }
